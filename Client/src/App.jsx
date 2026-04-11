1d7c717389ecaac155d57f93e4ddcfa38577ab30
Thakur-ji-5513 <rudrakshthakur60@gmail.com>
2026-04-11 14:07:09 +0530

    Name for login page fixed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import CheckPrice from "./pages/CheckPrice";
 import Signup from "./pages/Signup";
 
